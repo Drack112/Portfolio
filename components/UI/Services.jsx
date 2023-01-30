06987a45d8a56aed9a136c06eb23b8bb0db8f221
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-01-30 13:30:15 -0300

    final: services section updated

--- components/UI/Services.jsx
+++ components/UI/Services.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Container, Row, Col } from "reactstrap";
-import SectionSubtitle from "./SectionSubtitle";
-import classes from "../../styles/services.module.css";
-import ServicesItem from "./ServicesItem";
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import SectionSubtitle from './SectionSubtitle'
+import classes from '../../styles/services.module.css'
+import ServicesItem from './ServicesItem'
 
 const Services = () => {
   return (
@@ -28,17 +28,20 @@ const Services = () => {
             <h3 className="mb-0 mt-1">Responsivo</h3>
             <h3 className="mb-4">Escalável</h3>
             <h3 className="mb-4">E de fácil manutenção</h3>
+
             <p>
-              Tanto APIs REST simples quanto aplicativos integrados a Logger,
-              Cache, autenticação, código limpo e sempre focado ao
-              desenvolvimento em equipe usando ferramentas para auxiliar em
-              versionamento, gestor de dependências e outros serviços.
+              Tanto API's REST / GraphQL quanto aplicativos integrados a
+              sistemas baseados em micro-serviços, podendo esses conter Cache,
+              entrega de dados por gRPC, autenticação, gerenciamento de banco de
+              dados e sempre focado ao desenvolvimento em equipe usando
+              ferramentas para auxiliar em versionamento, gestor de dependências
+              e outros serviços.
             </p>
           </Col>
         </Row>
       </Container>
     </section>
-  );
-};
+  )
+}
 
-export default Services;
+export default Services
